c107ad597ebc17a3b5c25ab57d2fd133f06ab783
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-07 22:18:36 +0300

    place Logo in cell of grid

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -4,13 +4,20 @@ import { FC } from 'react';
 import Logo from '@/src/components/ui/Logo/Logo';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 
+import styles from './Header.module.scss';
 import type { HeaderProps } from './Header.props';
 
 const Header: FC<HeaderProps> = ({}) => {
   return (
-    <header>
-      <UiContainer>
-        <Logo />
+    <header className={cn(styles.appHeader)}>
+      <UiContainer className={cn(styles.grid)}>
+        <article className={cn(styles.cell)}></article>
+
+        <article className={cn(styles.cell)}>
+          <Logo />
+        </article>
+
+        <article className={cn(styles.cell)}></article>
       </UiContainer>
     </header>
   );
