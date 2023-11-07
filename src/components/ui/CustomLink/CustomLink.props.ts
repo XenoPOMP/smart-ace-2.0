c236b6b552be73e85afa4eb9a60ea23e378fbb34
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-07 21:44:31 +0300

    add comment to applyStyles property

--- src/components/ui/CustomLink/CustomLink.props.ts
+++ src/components/ui/CustomLink/CustomLink.props.ts
@@ -1,3 +1,9 @@
 export interface CustomLinkProps {
+  /**
+   * Defines if custom styles will be applied to
+   * link or not (like color).
+   *
+   * @default true
+   */
   applyStyles?: boolean;
 }
