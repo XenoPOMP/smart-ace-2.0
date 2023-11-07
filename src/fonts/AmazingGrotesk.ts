2fd2f21ce7ea9ab5ec289f612aa3e04de9df82c9
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-07 21:32:52 +0300

    add Amazing Grotesk font

---
+++ src/fonts/AmazingGrotesk.ts
@@ -0,0 +1,10 @@
+import localFont from 'next/font/local';
+
+export const AmazingGrotesk = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Amazing Grotesk/Amazing Grotesk Ultra.otf',
+      weight: '900',
+    },
+  ],
+});
