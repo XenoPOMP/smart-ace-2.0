761b7a3cef2697d70ac0aa4839ccaab9e0eb7a69
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-13 23:49:53 +0300

    create design for comment

--- src/components/blocks/CommentBlock/CommentBlock.tsx
+++ src/components/blocks/CommentBlock/CommentBlock.tsx
@@ -1,52 +1,59 @@
+import { VariableFC } from '@xenopomp/advanced-types';
 import cn from 'classnames';
 import { FC } from 'react';
+import TextOverflow from 'react-text-overflow';
 
 import { servicesData } from '@/src/data/services.data';
 
+import styles from './CommentBlock.module.scss';
 import type { CommentBlockProps } from './CommentBlock.props';
 
-const CommentBlock: FC<CommentBlockProps> = ({ comment }) => {
+const CommentBlock: VariableFC<'article', CommentBlockProps, 'children'> = ({
+  comment,
+  className,
+  ...props
+}) => {
   const { id, name, comment: text, rating, serviceId, uuid } = comment;
 
   const generatedAvatarPath = `https://api.dicebear.com/7.x/identicon/svg?seed=${name}`;
 
   return (
-    <div className={cn('mb-[1em]')}>
-      <div>
-        Comment with ID{' '}
-        <b>
-          <i>{id}</i>
-        </b>
-      </div>
-
-      <div>
-        Written by{' '}
-        <b>
-          <i>
-            {name} ({uuid ?? 'NO_UUID'})
-          </i>
-        </b>
-      </div>
-
-      <div>
+    <article className={cn('', styles.commentBlock, className)} {...props}>
+      <aside
+        style={{
+          width: 'max-content',
+        }}
+      >
         <img
           src={generatedAvatarPath}
           alt={`${name}\`s avatar`}
           style={{
-            width: 'min(25%, 2em)',
+            width: '2.5em',
           }}
         />
-      </div>
-
-      <div>{text}</div>
+      </aside>
 
       <div>
-        Referred to service{' '}
-        {servicesData.find(ser => ser.id === serviceId)?.title}
+        <header>
+          <h2 className={cn('text-[1.25em]')}>
+            <strong>
+              <TextOverflow text={name ?? 'Аноним'} />
+            </strong>
+          </h2>
+        </header>
+
+        <p
+          className={cn('')}
+          style={{
+            lineHeight: '1em',
+          }}
+        >
+          {text}
+        </p>
       </div>
 
-      <div>Rating: {rating} / 5</div>
-    </div>
+      <aside>Rating: {rating} / 5</aside>
+    </article>
   );
 };
 
