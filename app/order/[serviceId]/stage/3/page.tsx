c7bd02bb0326ab1f487dd4a9c72b710f83ae0aac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-11-17 21:51:02 +0300

    create StateSuspense

--- app/order/[serviceId]/stage/3/page.tsx
+++ app/order/[serviceId]/stage/3/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
 import { WithParams, WithSearchParams } from '@xenopomp/advanced-types';
 import cn from 'classnames';
 import { FC } from 'react';
 
 import ContactForm from '@/src/components/ui/ContactForm/ContactForm';
+import Loader from '@/src/components/ui/Loader/Loader';
+import StateSuspense from '@/src/components/ui/StateSuspense/StateSuspense';
 import UiContainer from '@/src/components/ui/UiContainer/UiContainer';
 import { servicesData } from '@/src/data/services.data';
+import useBoolean from '@/src/hooks/useBoolean';
 import { isDev } from '@/src/utils/isDev';
 
 const FinalStagePage: FC<
   WithSearchParams<WithParams<{}, 'serviceId'>, 'name' | 'email'>
 > = ({ params, searchParams }) => {
+  const [isSending, toggleIsSending, setIsSending] = useBoolean(false);
+
   const { serviceId } = params;
   const { name, email } = searchParams;
 
@@ -37,15 +44,28 @@ const FinalStagePage: FC<
         </>
       )}
 
-      <ContactForm
-        caption={currentService?.title}
-        subCaption={
-          'Все готово!\n' + 'Совсем скоро наш менеджер свяжется с вами.'
+      <StateSuspense
+        isLoading={isSending}
+        fallback={
+          <div
+            className={cn(
+              'w-full h-full flex justify-center items-center text-ord-font-primary'
+            )}
+          >
+            <Loader />
+          </div>
         }
-        links={{
-          middle: '/',
-        }}
-      />
+      >
+        <ContactForm
+          caption={currentService?.title}
+          subCaption={
+            'Все готово!\n' + 'Совсем скоро наш менеджер свяжется с вами.'
+          }
+          links={{
+            middle: '/',
+          }}
+        />
+      </StateSuspense>
     </>
   );
 };
---
+++ src/components/ui/StateSuspense/StateSuspense.props.ts
@@ -0,0 +1,3 @@
+export interface StateSuspenseProps {
+  isLoading: boolean;
+}
